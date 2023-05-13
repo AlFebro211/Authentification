0afa82cd3598079647fc97d504b23be870774da9
NdayiragijeAlainFabrice <alainfabbricendayiragije98@gmail.com>
2023-05-13 13:40:35 +0200

    add_hshpass

--- Authentification/Authentification/info.py
+++ Authentification/Authentification/info.py
@@ -1,6 +1,6 @@
 EMAIL_USE_TLS =True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER ='alfebro211@gmail.com'
-EMAIL_HOST_PASSWORD = 'password'
+EMAIL_HOST_PASSWORD = 'vhcndomtoifqvrkf'
 EMAIL_PORT=587
 
